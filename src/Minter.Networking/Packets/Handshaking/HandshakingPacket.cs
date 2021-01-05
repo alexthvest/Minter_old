806b27af9c64427a9d186beb0f45bf180964330e
alexthvest <alexthvest@gmail.com>
2021-01-05 14:25:53 +0300

    HandshakingPacket type changed to record

--- src/Minter.Networking/Packets/Handshaking/HandshakingPacket.cs
+++ src/Minter.Networking/Packets/Handshaking/HandshakingPacket.cs
@@ -1,40 +1,4 @@
 namespace Minter.Networking.Packets.Handshaking
 {
-    public class HandshakingPacket : IPacket
-    {
-        /// <summary>
-        /// Contains protocol version (minecraft version)
-        /// </summary>
-        public int ProtocolVersion { get; }
-        
-        /// <summary>
-        /// Contains server hostname
-        /// </summary>
-        public string Hostname { get; }
-        
-        /// <summary>
-        /// Contains server port (default 25565)
-        /// </summary>
-        public int Port { get; }
-        
-        /// <summary>
-        /// Contains next connection state
-        /// </summary>
-        public ConnectionState NextState { get; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="protocolVersion"></param>
-        /// <param name="hostname"></param>
-        /// <param name="port"></param>
-        /// <param name="nextState"></param>
-        public HandshakingPacket(int protocolVersion, string hostname, int port, ConnectionState nextState)
-        {
-            ProtocolVersion = protocolVersion;
-            Hostname = hostname;
-            Port = port;
-            NextState = nextState;
-        }
-    }
+    public record HandshakingPacket(int ProtocolVersion, string Hostname, int Port, ConnectionState NextState) : IPacket;
 }
