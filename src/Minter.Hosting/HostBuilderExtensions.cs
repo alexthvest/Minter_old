27cbf74d61691636349c4e10a948c5f334f36657
alexthvest <alexthvest@gmail.com>
2021-01-12 21:33:50 +0300

    added: hosted service and host builder minter extension

---
+++ src/Minter.Hosting/HostBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Minter.Hosting
+{
+    public static class HostBuilderExtensions
+    {
+        public static IHostBuilder ConfigureMinterDefaults(this IHostBuilder hostBuilder)
+        {
+            hostBuilder.ConfigureServices(services =>
+            {
+                services.AddHostedService<ServerHostedService>();
+            });
+
+            return hostBuilder;
+        }
+    }
+}
---
+++ src/Minter.Hosting/ServerHostedService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Minter.Hosting
+{
+    public class ServerHostedService : IHostedService
+    {
+        private readonly ILogger<ServerHostedService> _logger;
+
+        public ServerHostedService(ILogger<ServerHostedService> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        private void StartServer()
+        {
+            _logger.LogInformation("Server is started");
+        }
+        
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Task.Run(StartServer, cancellationToken);
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
