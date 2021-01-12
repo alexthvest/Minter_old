b3d87aff3869270ab94abd8f50e9b5200a56cf3b
alexthvest <alexthvest@gmail.com>
2021-01-12 21:18:36 +0300

    Initial commit

---
+++ src/Minter.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Minter.Example
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Minter!");
+        }
+    }
+}
